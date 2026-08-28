b0bbd96087ef885d91c3557956cc3ba783980817
IncertiHenri <henrique_incerti@estudante.sesisenai.org.br>
2026-08-28 11:53:56 -0300

    add - inicio cadastro

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Ifood</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    
+
+    <h1>Cadastro de clientes</h1>
+
+    <form action="public/cadastro_cliente.php" method="POST">
+        <label for="nome">Nome do cliente:</label>
+        <input type="text" name="nome">
+        <label for="fone">Telefone do cliente:</label>
+        <input type="text" name="fone">
+        <label for="email">E-mail do cliente:</label>
+        <input type="email" name="email">
+        <label for="end">Endereço do cliente:</label>
+        <input type="text" name="end">
+        <button type="submit">Cadastrar</button>
+    </form>
+
 </body>
 </html>
---
+++ public/cadastro_cliente.php
@@ -0,0 +1,5 @@
+<?php
+
+include("../infra/conexao.php");
+
+?>
